ff76c5c649d7d2853a805ea7223b7072ece55253
shahrukh-nuverse <shahrukh@nuverse.in>
2026-01-07 18:27:28 +0530

    code update app restructured

--- app/src/main/java/com/vital_self/enums/LoginEnum.kt
+++
@@ -1,7 +0,0 @@
-package com.vital_self.enums
-
-enum class LoginEnum {
-    INVALID_USERNAME,
-    INVALID_PASSWORD,
-    VALID
-}
--- app/src/main/java/com/vital_self/enums/RegisterEnum.kt
+++
@@ -1,8 +0,0 @@
-package com.vital_self.enums
-
-enum class RegisterEnum {
-    INVALID_USERNAME,
-    INVALID_EMAIL,
-    INVALID_PASSWORD,
-    VALID
-}
